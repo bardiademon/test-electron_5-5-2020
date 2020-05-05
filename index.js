0831ee9e6a7cccb4d1651451d5b5b57f873207a9
bardiademon <bardiademon@gmail.com>
2020-05-05 12:50:02 +0430

    frame reload , add electron reload

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
 import { app , BrowserWindow } from 'electron';
+import url from 'url';
+import path from 'path';
+
+import ElectronReload from 'electron-reload';
+
+ElectronReload (__dirname);
 
 app.on ("ready" , () =>
 {
-    new BrowserWindow ({ width : 500 , height : 500 });
+    const frame = new BrowserWindow ({ width : 500 , height : 500 });
+    let promise = frame.loadURL (url.format ({
+        pathname : path.join (__dirname , "index.html") ,
+        protocol : "file:"
+    }));
+
+    promise.then (r =>
+    {
+        console.log (r);
+    })
 });
